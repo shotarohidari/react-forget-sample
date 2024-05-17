b479d829ce49e889a3351bc609c1945ba4ce4e97
左翔太朗 <s.hidaridesu@gmail.com>
2024-05-17 11:36:30 +0900

    react-compilerのeslintpluginの追加

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@
 import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
 import reactHooks from "eslint-plugin-react-hooks"
+import reactCompiler from "eslint-plugin-react-compiler"
 
 export default [
   ...tseslint.config(
@@ -12,11 +13,20 @@ export default [
   {
     files: ["**/*.jsx", "**/*.tsx"],
     plugins: {
-        "react-hooks":reactHooks
+      "react-hooks": reactHooks,
     },
     rules: {
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  {
+    files: ["**/*.jsx", "**/*.tsx"],
+    plugins: {
+      "react-compiler": reactCompiler,
+    },
+    rules: {
+      "react-compiler/react-compiler": "error",
     },
   },
 ]
